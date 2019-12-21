c59ff24a17325298bf1c3edf10d3f9d038ddc5ae
Hassam Riaz <45106260+thehassamriaz@users.noreply.github.com>
2019-12-21 20:12:05 +0500

    updated code

--- app/src/main/java/com/hassam/travellingbuddy/UsersActivity.java
+++ app/src/main/java/com/hassam/travellingbuddy/UsersActivity.java
@@ -109,30 +109,6 @@ public class UsersActivity extends AppCompatActivity {
 
 
 }
-class UsersViewHolder extends RecyclerView.ViewHolder{
-
-    View mView;
-
-    TextView name;
-    TextView aboutMe;
-    CircleImageView image;
-
-    public UsersViewHolder(@NonNull View itemView) {
-        super(itemView);
-
-        mView = itemView;
-
-        name = itemView.findViewById(R.id.username);
-        aboutMe = itemView.findViewById(R.id.status);
-        image = itemView.findViewById(R.id.profileimage);
-
-
-
-    }
-
-
-
-}
 
 
 
---
+++ app/src/main/java/com/hassam/travellingbuddy/UsersViewHolder.java
@@ -0,0 +1,34 @@
+package com.hassam.travellingbuddy;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+class UsersViewHolder extends RecyclerView.ViewHolder{
+
+    View mView;
+
+    TextView name;
+    TextView aboutMe;
+    CircleImageView image;
+
+    public UsersViewHolder(@NonNull View itemView) {
+        super(itemView);
+
+        mView = itemView;
+
+        name = itemView.findViewById(R.id.username);
+        aboutMe = itemView.findViewById(R.id.status);
+        image = itemView.findViewById(R.id.profileimage);
+
+
+
+    }
+
+
+
+}
