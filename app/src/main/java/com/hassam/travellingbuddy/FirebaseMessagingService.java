3ea700044f1e687932058187baf65dd4276a25a8
Hassam Riaz <45106260+thehassamriaz@users.noreply.github.com>
2020-01-01 01:13:35 +0500

    Push Notification Completed

--- app/src/main/java/com/hassam/travellingbuddy/FirebaseMessagingService.java
+++ app/src/main/java/com/hassam/travellingbuddy/FirebaseMessagingService.java
@@ -12,7 +12,7 @@ import com.google.firebase.messaging.RemoteMessage;
 
 public class FirebaseMessagingService extends com.google.firebase.messaging.FirebaseMessagingService {
 
-
+//
     @Override
     public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
