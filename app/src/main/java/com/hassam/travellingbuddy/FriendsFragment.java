9d438a47f35e1701d2c36c5f9b4c2bbd69744268
Hassam Riaz <45106260+thehassamriaz@users.noreply.github.com>
2020-01-02 21:05:47 +0500

    Code Updated

--- app/src/main/java/com/hassam/travellingbuddy/FriendsFragment.java
+++ app/src/main/java/com/hassam/travellingbuddy/FriendsFragment.java
@@ -1,9 +1,11 @@
 package com.hassam.travellingbuddy;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,9 +13,15 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.firebase.ui.database.SnapshotParser;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.squareup.picasso.Picasso;
 
 public class FriendsFragment extends Fragment {
 
@@ -29,7 +37,6 @@ public class FriendsFragment extends Fragment {
         //Required empty public constructor
     }
 
-    @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ( (HomeActivity) getActivity()).changetitle("Friends");
@@ -48,4 +55,60 @@ public class FriendsFragment extends Fragment {
 //      Inflate the layout for this fragment
         return mMainView;
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        Query query = FirebaseDatabase.getInstance().getReference().child("UserInfo");
+        FirebaseRecyclerOptions<User> options = new FirebaseRecyclerOptions.Builder<User>().setQuery(query, new SnapshotParser<User>() {
+            @NonNull
+            @Override
+            public User parseSnapshot(@NonNull DataSnapshot snapshot) {
+                return new User (
+
+                        snapshot.child("name").getValue(String.class),
+                        snapshot.child("image").getValue(String.class),
+                        snapshot.child("aboutMe").getValue(String.class)
+                );
+            }
+        }).build();
+
+        //------------HOLDER TO LOAD FILES
+        FirebaseRecyclerAdapter<User, FriendsViewHolder> adapter = new FirebaseRecyclerAdapter<User, FriendsViewHolder>(options) {
+
+            @NonNull
+            @Override
+            public FriendsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(getContext()).inflate(R.layout.users_single_layout, parent, false);
+                return new FriendsViewHolder(view);
+            }
+
+            @Override
+            protected void onBindViewHolder(@NonNull FriendsViewHolder holder, int position, @NonNull User model) {
+                holder.name.setText(model.name);
+                Picasso.get().load(model.image).placeholder(R.drawable.profile_image).into(holder.image);
+                holder.aboutMe.setText(model.aboutMe);
+
+                final String current_userID = getRef(position).getKey();
+
+                holder.mView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        Intent profile_intent = new Intent(getContext(), ProfileActivity.class);
+                        profile_intent.putExtra("current_userID", current_userID);
+                        startActivity(profile_intent);
+                    }
+                });
+            }
+        };
+        mFriendsList.setAdapter(adapter);
+        adapter.startListening();
+
+
+    }
+
+
+
 }
