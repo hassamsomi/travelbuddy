ea006866aa28ae93094c3f53984229c57d12feb6
Hassam Riaz <45106260+thehassamriaz@users.noreply.github.com>
2020-01-06 19:51:23 +0500

    online offline feature

--- app/src/main/java/com/hassam/travellingbuddy/ETravel.java
+++ app/src/main/java/com/hassam/travellingbuddy/ETravel.java
@@ -2,13 +2,23 @@ package com.hassam.travellingbuddy;
 
 import android.app.Application;
 
+import androidx.annotation.NonNull;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.OkHttp3Downloader;
 import com.squareup.picasso.Picasso;
 
 public class ETravel {
 
     public class Etravel extends Application{
+
+        private DatabaseReference mUserDatabase;
+        private FirebaseAuth mAuth;
 
         @Override
         public void onCreate() {
@@ -25,6 +35,26 @@ public class ETravel {
             built.setLoggingEnabled(true);
             Picasso.setSingletonInstance(built);
 
+            mAuth = FirebaseAuth.getInstance();
+            mUserDatabase = FirebaseDatabase.getInstance().getReference().child("UserInfo").child(mAuth.getCurrentUser().getUid());
+
+            mUserDatabase.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                    if(dataSnapshot != null) {
+
+                        mUserDatabase.child("online").onDisconnect().setValue(false);
+                        mUserDatabase.child("online").setValue(true);
+                    }
+
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
 
         }
     }
--- app/src/main/java/com/hassam/travellingbuddy/MainActivity.java
+++ app/src/main/java/com/hassam/travellingbuddy/MainActivity.java
@@ -3,15 +3,19 @@ package com.hassam.travellingbuddy;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Menu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    private DatabaseReference mUserRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,37 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mAuth = FirebaseAuth.getInstance();
+        mUserRef = FirebaseDatabase.getInstance().getReference().child("UserInfo").child(mAuth.getCurrentUser().getUid());
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //It will check if user signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if(currentUser == null){
+
+            sendtoStart();
+
+        }
+        else{
+            mUserRef.child("online").setValue(true);
+
+        }
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        mUserRef.child("online").setValue(false);
+    }
+
+    private void sendtoStart(){
 
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
@@ -37,6 +72,8 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }, 2000);
+
     }
+
 }
 
