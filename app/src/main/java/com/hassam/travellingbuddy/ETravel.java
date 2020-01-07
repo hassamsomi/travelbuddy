c15d99e64c2669678ad50dc1fe48bc5810503a9a
Hassam Riaz <45106260+thehassamriaz@users.noreply.github.com>
2020-01-08 02:32:22 +0500

    online offline feature

--- app/src/main/java/com/hassam/travellingbuddy/ETravel.java
+++ app/src/main/java/com/hassam/travellingbuddy/ETravel.java
@@ -9,36 +9,51 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ServerValue;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.OkHttp3Downloader;
 import com.squareup.picasso.Picasso;
 
-public class ETravel {
+public class ETravel extends Application {
 
-    public class Etravel extends Application{
+    private FirebaseAuth mAuth;
+    private DatabaseReference mUserDatabase;
 
-        private DatabaseReference mUserDatabase;
-        private FirebaseAuth mAuth;
+    @Override
+    public void onCreate() {
+        super.onCreate();
 
-        @Override
-        public void onCreate() {
-            super.onCreate();
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
 
-            FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+        //PICASSO
 
-            //PICASSO
+        Picasso.Builder builder = new Picasso.Builder(this);
+        builder.downloader(new OkHttp3Downloader(this ,Integer.MAX_VALUE));
+        Picasso built = builder.build();
+        built.setIndicatorsEnabled(true);
+        built.setLoggingEnabled(true);
+        Picasso.setSingletonInstance(built);
 
-            Picasso.Builder builder = new Picasso.Builder(this);
-            builder.downloader(new OkHttp3Downloader(this ,Integer.MAX_VALUE));
-            Picasso built = builder.build();
-            built.setIndicatorsEnabled(true);
-            built.setLoggingEnabled(true);
-            Picasso.setSingletonInstance(built);
+        mAuth = FirebaseAuth.getInstance();
+        mUserDatabase = FirebaseDatabase.getInstance().getReference().child("UserInfo").child(mAuth.getCurrentUser().getUid());
+        mUserDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
-            mAuth = FirebaseAuth.getInstance();
+                if(dataSnapshot != null){
+
+                    mUserDatabase.child("online").onDisconnect().setValue(ServerValue.TIMESTAMP);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
 
-        }
     }
 
 }
