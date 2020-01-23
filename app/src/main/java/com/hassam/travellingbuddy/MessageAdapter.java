d9f26706853166c72e0e55660a70dd0e8f786804
Hassam Riaz <45106260+thehassamriaz@users.noreply.github.com>
2020-01-23 13:59:49 +0500

    Image Process Completed

--- app/src/main/java/com/hassam/travellingbuddy/MessageAdapter.java
+++ app/src/main/java/com/hassam/travellingbuddy/MessageAdapter.java
@@ -1,13 +1,10 @@
 package com.hassam.travellingbuddy;
 
-import android.graphics.Color;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -21,7 +18,6 @@ import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
-import java.util.Objects;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -51,19 +47,20 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MessageV
 
     public class MessageViewHolder extends RecyclerView.ViewHolder{
 
-        public TextView messageText, displaytext;
-        public CircleImageView profileImage;
-        public ImageView messageImage;
+        public TextView senderText, senderUserName, receiverText, receiverUserName;
+        public ImageView senderImage, receiverImage;
 
         public MessageViewHolder (@NonNull View itemView){
 
             super(itemView);
-            messageText = itemView.findViewById(R.id.message_text_layout);
-            profileImage = itemView.findViewById(R.id.message_profile_layout);
-            displaytext = itemView.findViewById(R.id.user_text_layout);
-            messageImage = itemView.findViewById(R.id.message_image_layout);
-
-
+//          SENDER LAYOUT
+            senderText = itemView.findViewById(R.id.sender_text_layout);
+            senderUserName = itemView.findViewById(R.id.sender_username);
+            senderImage = itemView.findViewById(R.id.sender_image_layout);
+//          RECEIVER LAYOUT
+            receiverText = itemView.findViewById(R.id.receiver_text_layout);
+            receiverUserName = itemView.findViewById(R.id.receiver_username);
+            receiverImage = itemView.findViewById(R.id.receiver_image_layout);
         }
     }
     @Override
@@ -83,10 +80,8 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MessageV
                 public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
                     String name = dataSnapshot.child("name").getValue().toString();
-                    String image = dataSnapshot.child("image").getValue().toString();
 
-                    holder.displaytext.setText(name);
-                    Picasso.get().load(image).placeholder(R.drawable.profile_image).into(holder.profileImage);
+                    holder.senderUserName.setText(name);
 
                 }
 
@@ -99,15 +94,15 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MessageV
             if(fromUserType.equals("text"))
             {
 
-                holder.messageText.setText(c.getMessage());
-                holder.messageText.setVisibility(View.VISIBLE);
+                holder.senderText.setText(c.getMessage());
+                holder.senderText.setVisibility(View.VISIBLE);
 
             }
             else if(fromUserType.equals("image"))
             {
 
-                holder.messageImage.setVisibility(View.VISIBLE);
-                Picasso.get().load(c.getMessage()).into(holder.messageImage);
+                holder.senderImage.setVisibility(View.VISIBLE);
+                Picasso.get().load(c.getMessage()).into(holder.senderImage);
 
             }
 
